9c4e4a9581bbb9dc42d89db3e869e831c665c284
Konstantin Vasilev <swtormy@yahoo.com>
2022-12-29 23:40:36 +0300

    nv

--- frontend/src/pages/Sorter.jsx
+++ frontend/src/pages/Sorter.jsx
@@ -3,7 +3,7 @@ import { Reorder } from 'framer-motion'
 
 const Sorter = () => {
     const [elements, setElements] = useState([])
-
+    const [task, setTask] = useState('Put the sentences in the correct order')
     const splitText = (e) => {
         const segmenter = new Intl.Segmenter(
             'en', { granularity: 'sentence' }
@@ -17,8 +17,8 @@ const Sorter = () => {
             s => s.segment
         )
 
-        
-        
+
+
         setElements(textArray)
 
         // var arrWords = []
@@ -31,17 +31,17 @@ const Sorter = () => {
 
     return (
         <>
-            
+
             <div className='container mx-auto px-4 flex flex-col min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 text-sm'>
                 < div className='text-2xl font-bold pb-2' >
-                    <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-6 dark:bg-gray-800 dark:border-gray-700">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your sentences</label>
-                        <textarea  onChange={splitText}  id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-                        <br/>
+                    <div className="w-full w-[30rem] p-4 bg-white border rounded-lg shadow-md sm:p-6 dark:bg-gray-800 dark:border-gray-700">
+                        <h5 htmlFor="message" className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">Your sentences</h5>
+                        <textarea onChange={splitText} id="message" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write a sentence or two..."></textarea>
+                        <br />
                         <h5 className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
-                            Connect wallet
+                            Task:
                         </h5>
-                        <p className="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p>
+                        <input className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4" onChange={(e) => setTask(e.target.value)} value={task} placeholder='Describe the assignment' />
 
 
                         <Reorder.Group as='ul' axys='y' values={elements} onReorder={setElements}>
@@ -83,10 +83,11 @@ const Sorter = () => {
                             </li> */}
                         </Reorder.Group>
 
-                        <div>
+                        <div className='flex justify-between mt-2'>
                             <a href="#" className="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
                                 <svg className="w-3 h-3 mr-2" aria-hidden="true" focusable="false" data-prefix="far" data-icon="question-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 448c-110.532 0-200-89.431-200-200 0-110.495 89.472-200 200-200 110.491 0 200 89.471 200 200 0 110.53-89.431 200-200 200zm107.244-255.2c0 67.052-72.421 68.084-72.421 92.863V300c0 6.627-5.373 12-12 12h-45.647c-6.627 0-12-5.373-12-12v-8.659c0-35.745 27.1-50.034 47.579-61.516 17.561-9.845 28.324-16.541 28.324-29.579 0-17.246-21.999-28.693-39.784-28.693-23.189 0-33.894 10.977-48.942 29.969-4.057 5.12-11.46 6.071-16.666 2.124l-27.824-21.098c-5.107-3.872-6.251-11.066-2.644-16.363C184.846 131.491 214.94 112 261.794 112c49.071 0 101.45 38.304 101.45 88.8zM298 368c0 23.159-18.841 42-42 42s-42-18.841-42-42 18.841-42 42-42 42 18.841 42 42z"></path></svg>
-                                Why do I need to connect with my wallet?</a>
+                                How do I work with the app?</a>
+                                <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Create Task</button>
                         </div>
                     </div>
                 </div>
